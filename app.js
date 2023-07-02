8cf810bf26ee5ec7b04df515b93202d17e24dcdd
rebeccaliii <rebeccali823@gmail.com>
2023-07-02 17:35:32 +0800

    install hbs

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+// 載入handlebars
+const exphbs = require('express-handlebars')
+
+// 設定樣板引擎
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
